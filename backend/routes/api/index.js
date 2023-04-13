ed8c3b324090279e3776ccdd500ace75a20a24d9
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-13 16:57:04 -0400

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const { restoreUser } = require('../../utils/auth.js');
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use(restoreUser);
+
+// // POST /api/test
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
