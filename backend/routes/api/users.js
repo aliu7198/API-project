3fd2025e3b57af95becad086686375b63b739854
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-14 10:42:22 -0400

    Add first & last name to User model

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -27,13 +27,15 @@ const validateSignup = [
 ];
 
 // Sign up
-router.post("", validateSignup, async (req, res) => {
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, username } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ email, username, hashedPassword });
 
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };
