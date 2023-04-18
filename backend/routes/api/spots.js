4d28f6ee8be658bccefddbdf26e98c902642a20a
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-18 18:29:51 -0400

    Complete GET /api/spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -23,25 +23,25 @@ router.get("/", async (req, res, next) => {
 
   spotsArr.forEach(async (spot) => {
     if (spot.Reviews.length) {
-        let sum = 0;
-        spot.Reviews.forEach(review => {
-            sum += review.stars
-        })
-        spot.avgRating = sum / spot.Reviews.length;
+      let sum = 0;
+      spot.Reviews.forEach((review) => {
+        sum += review.stars;
+      });
+      spot.avgRating = sum / spot.Reviews.length;
     }
 
     if (spot.SpotImages.length) {
-        spot.SpotImages.forEach(image => {
-            if (image.preview) {
-                spot.previewImage = image.url
-            }
-        });
+      spot.SpotImages.forEach((image) => {
+        if (image.preview) {
+          spot.previewImage = image.url;
+        }
+      });
     }
     delete spot.Reviews;
     delete spot.SpotImages;
-});
+  });
 
-  return res.json({Spots: spotsArr});
+  return res.json({ Spots: spotsArr });
 });
 
 module.exports = router;
