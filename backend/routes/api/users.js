bc9f0f2cd55139df47e3e74fbaadf651435f7972
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-18 13:35:13 -0400

    Debug user sign-up  route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,7 +28,7 @@ const validateSignup = [
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-  const { firstName, lastName, email, password, username } = req.body;
+  const { firstName, lastName, email, password, username } = req.body; // fixed this
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
