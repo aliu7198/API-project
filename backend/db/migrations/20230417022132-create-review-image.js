2f88e4013aa6c60ad564830c857aa1ca065d9ddd
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-19 14:11:45 -0400

    Update migrations & models

--- backend/db/migrations/20230417022132-create-review-image.js
+++ backend/db/migrations/20230417022132-create-review-image.js
@@ -22,7 +22,8 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
