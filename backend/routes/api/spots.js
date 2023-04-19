81531265719414e0d9372d027738ec3e81ddc639
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-19 12:44:58 -0400

    Complete POST /api/spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,37 +1,17 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
-
+const { check } = require("express-validator");
 const { restoreUser, requireAuth } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
 const { Spot, Review, SpotImage, User } = require("../../db/models");
 
-const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
-
 const router = express.Router();
+/*****************************************************************************/
 
 // GET /spots/current
 router.get("/current", restoreUser, requireAuth, async (req, res, next) => {
   const { user } = req;
-
-  // Alternate Solution w/ raw:true in options, check with TL on how this works
-
-  //   const spots = await Spot.findAll({
-  //     where: {
-  //       ownerId: user.dataValues.id,
-  //     },
-  //     raw: true,
-  //   });
-
-  //   spots.forEach(async (spot) => {
-  //     const sumStars = await Review.sum("stars", { where: { spotId: spot.id } });
-  //     const totalReviews = await Review.count({ where: { spotId: spot.id } });
-  //     spot.avgRating = sumStars / totalReviews;
-  //     // console.log(spot);
-  //   });
-
-  // console.log(spots);
-  // return res.json({ Spots: spots });
 
   const spots = await Spot.findAll({
     where: {
@@ -70,7 +50,7 @@ router.get("/current", restoreUser, requireAuth, async (req, res, next) => {
 });
 
 // GET /spots/:spotId
-router.get("/:spotId", async (req, res, next) => {
+router.get("/:spotId", async (req, res) => {
   const spot = await Spot.findByPk(req.params.spotId, {
     include: [
       { model: Review },
@@ -101,7 +81,7 @@ router.get("/:spotId", async (req, res, next) => {
 });
 
 // GET /spots
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
   const spots = await Spot.findAll({
     include: [{ model: Review }, { model: SpotImage }],
   });
@@ -134,4 +114,58 @@ router.get("/", async (req, res, next) => {
   return res.json({ Spots: spotsArr });
 });
 
+// POST /spots
+
+// Validator for Spot
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -90, max: 90 })
+    .withMessage("Latitude is not valid"),
+  check("lng")
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -180, max: 180 })
+    .withMessage("Longitude is not valid"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
+
+router.post("/", requireAuth, restoreUser, validateSpot, async (req, res) => {
+  const { user } = req;
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+  const newSpot = await Spot.create({
+    ownerId: user.dataValues.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
+
+  return res.status(201).json(newSpot);
+});
+
+/*****************************************************************************/
 module.exports = router;
