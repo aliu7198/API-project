fe8aaab5833cf16697456bf0ef35940b1bb7e2a1
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-21 19:04:47 -0400

    Complete DELETE /api/spot-images/:imageId

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { check } = require("express-validator");
+const { requireAuth } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
+const { Spot, Review, SpotImage, User, ReviewImage } = require("../../db/models");
+
+const router = express.Router();
+/*****************************************************************************/
+
+// Delete a Spot Image
+// DELETE /spot-images/:imageId
+router.delete("/:imageId", requireAuth, async (req, res) => {
+  const { user } = req;
+  const spotImage = await SpotImage.findByPk(req.params.imageId, { include: [{ model: Spot }] });
+
+  if (!spotImage) {
+    return res.status(404).json({
+      message: "Spot Image couldn't be found",
+    });
+  }
+
+  if (spotImage.Spot.ownerId !== user.id) {
+    return res.status(403).json({
+      message: "Forbidden",
+    });
+  }
+
+  await spotImage.destroy();
+  return res.json({
+    message: "Successfully deleted"
+  });
+});
+
+/*****************************************************************************/
+module.exports = router;
