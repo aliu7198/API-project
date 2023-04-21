106b8714e77c00c158271d50df7f7632cd55755f
Adanna Liu <adanna.c.liu@gmail.com>
2023-04-21 19:14:46 -0400

    Complete DELETE /api/review-images/:imageId endpoint

---
+++ backend/routes/api/review-images.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { check } = require("express-validator");
+const { requireAuth } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
+const { Spot, Review, SpotImage, User, ReviewImage } = require("../../db/models");
+
+const router = express.Router();
+/*****************************************************************************/
+
+// Delete a Review Image
+// DELETE /review-images/:imageId
+router.delete("/:imageId", requireAuth, async (req, res) => {
+  const { user } = req;
+  const reviewImage = await ReviewImage.findByPk(req.params.imageId, { include: [{ model: Review }] });
+
+  if (!reviewImage) {
+    return res.status(404).json({
+      message: "Review Image couldn't be found",
+    });
+  }
+
+  if (reviewImage.Review.userId !== user.id) {
+    return res.status(403).json({
+      message: "Forbidden",
+    });
+  }
+
+  await reviewImage.destroy();
+  return res.json({
+    message: "Successfully deleted",
+  });
+});
+
+/*****************************************************************************/
+module.exports = router;
