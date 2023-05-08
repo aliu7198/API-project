96072d2ba5290a38aee7c46c81b976f828479ccd
Adanna Liu <adanna.c.liu@gmail.com>
2023-05-08 18:44:32 -0400

    Create link to single spot

--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -1,23 +1,25 @@
-import "./SpotCard.css"
+import { Link } from "react-router-dom";
+import "./SpotCard.css";
 
 const SpotCard = ({ spot }) => {
-  console.log(spot);
   return (
-    <div id="spotcard-wrapper">
-      <img src={spot.previewImage} alt={spot.name}/>
-      <div id="line-1">
-        <span style={{ fontWeight: "bold" }}>
-          {spot.city}, {spot.state}
-        </span>
-        <span>
-          <i class="fa-solid fa-star"></i>
-          {spot.avgRating ? `  ${spot.avgRating}` : 'New'}
-        </span>
+    <Link to={`/api/spots/${spot.id}`} style={{ color: "black" }}>
+      <div id="spotcard-wrapper">
+        <img src={spot.previewImage} alt={spot.name} />
+        <div id="line-1">
+          <span style={{ fontWeight: "bold" }}>
+            {spot.city}, {spot.state}
+          </span>
+          <span>
+            <i class="fa-solid fa-star"></i>
+            {spot.avgRating ? `  ${spot.avgRating}` : "New"}
+          </span>
+        </div>
+        <p>
+          <span style={{ fontWeight: "bold" }}>${spot.price.toFixed(2)}</span> night
+        </p>
       </div>
-      <p>
-        <span style={{ fontWeight: "bold" }}>${spot.price.toFixed(2)}</span> night
-      </p>
-    </div>
+    </Link>
   );
 };
 
